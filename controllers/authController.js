b6cd73923141c1df05640bcc6d01b65d0d02eacc
unknown <adityaomar33@gmail.com>
2023-08-16 20:42:12 +0530

    added jwt and auth tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from '../models/userModel.js'
-import { hashPassword } from '../helpers/authHelper.js'
+import { comparePassword, hashPassword } from '../helpers/authHelper.js'
+import JWT from 'jsonwebtoken'
 const registerController = async (req, res) => {
     try {
         const { name, email, password, phone, address } = req.body
@@ -34,7 +35,7 @@ const registerController = async (req, res) => {
         //register user
         const hashedPassword = await hashPassword(password)
         //save
-        const user = new userModel({ name, email, phone, address, password: hashedPassword }).save()
+        const user = await new userModel({ name, email, phone, address, password: hashedPassword }).save()
 
         res.status(201).send({
             success: true,
@@ -53,3 +54,60 @@ const registerController = async (req, res) => {
     }
 }
 export { registerController }
+export const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        //validation
+        if (!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: 'Invalid email or password'
+            })
+        }
+
+        //check user
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                messaeg: 'Email is not registered'
+            })
+        }
+
+        const match = await comparePassword(password, user.password)
+        if (!match) {
+            return res.status(200).send({
+                success: false,
+                message: 'Invalid password'
+            })
+        }
+        //token 
+        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+        res.status(200).send({
+            success: true,
+            message: 'login successfully',
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address
+            },
+            token,
+        })
+
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({
+            success: false,
+            message: 'Error in login',
+            e
+        })
+    }
+}
+//test controller 
+export const testController = (req, res) => {
+    console.log('protected route');
+}
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,12 @@
+import JWT from "jsonwebtoken";
+
+//protected Riutes token base
+
+export const requireSignIn = async (req, res, next) => {
+    try {
+        const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+        next()
+    } catch (e) {
+        console.log(e);
+    }
+}
